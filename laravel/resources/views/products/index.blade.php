de6f451ead7ab23084ec24d0c5c4eab398951167
mcglonelevi <mcglonelevi@yahoo.com>
2017-10-14 11:17:13 -0400

    fix product index

--- laravel/resources/views/products/index.blade.php
+++ laravel/resources/views/products/index.blade.php
@@ -7,23 +7,21 @@
         {{ $products->links() }}
     </div>
     <div class="container product-display">
-        <div class="col-md-8 col-md-offset-2">
-            <table class="listing">
-              @foreach($products as $p)
-                <tr>
-                    <td>
-                      <img src="http://via.placeholder.com/50x50" alt="">
-                    </td>
-                    <td>
-                      <a href="{{ url('/products/' . $p->productCode) }}">{{$p->productName}}</a>
-                    </td>
-                    <td>
-                      <a href="{{ url('/products/' . $p->productCode) . }}">{{$p->productName}}</a>
-                    </td>
-                </tr>
-              @endforeach
-            </table>
-        </div>
+      <table class="listing">
+        @foreach($products as $p)
+          <tr>
+              <td>
+                <img src="http://via.placeholder.com/50x50" alt="">
+              </td>
+              <td>
+                <a href="{{ url('/products/' . $p->productCode) }}">{{$p->productName}}</a>
+              </td>
+              <td>
+                <a href="{{ url('/products/' . $p->productCode . '/addtocart') }}">Add to Cart</a>
+              </td>
+          </tr>
+        @endforeach
+      </table>
     </div>
     <div class="container">
         {{ $products->links() }}
