56f1951417b9fb24e7dd6e2123c8fc962a06ade0
Eric Davis <davis.25811@gmail.com>
2017-10-31 20:24:12 -0400

    Added static content to about page

--- laravel/resources/views/about.blade.php
+++ laravel/resources/views/about.blade.php
@@ -6,27 +6,21 @@
         <article>
             <div class="grid">
                 <div class="col-sm-12 about-us">
-                     <h2>Welcome</h2>
-                     <p>
-                             Welcome to Lugnutz computer parts store, your number one online computer store.
-                             Here at Lugnutz we are committed to customer satisfaction.
-                     </p>
-                       <h2>Our Goal</h2>
-                     <p>
-                             Our mission is to become a number one online computer store by delivering high
-                             quality goods and services that exceed the needs of our customer. We believe transparency
-                             is key to establishing and maintaining great customer satisfaction.
-                     </p>
-                     <h2>Our Mission</h2>
-                     <p>
-                             Our mission is to become a number one online computer store by delivering high
-                             quality goods and services that exceed the needs of our customer. We believe transparency
-                             is key to establishing and maintaining great customer satisfaction.
-                     </p>
-                 </div>
+                    <h2>Our History</h2>
+                    <p>
+                        Lugnutz Computer Parts has been a family-owned business for over 50 years. Founded by Larry Lugnut in 1965, Larry sought to establish a consumer electronics company focused on building quality relationships with its customers. Larry's devotion to customer satisfaction helped Lugnutz Computer Parts develop into a regional success story, and his vision is now being carried on by his granddaughter and current CEO, Patty Lugnut. 
+                    </p>
+                    <h2>Our Mission</h2>
+                    <p>
+                        Our mission is to build lasting customer relationships by delivering only highest quality products and services that exceed the expectations of our clients. For over 50 years, we have believed transparency is key to establishing and maintaining customer satisfaction. Whether you are in the market for a new PC or a single component, no order is too large nor too small for our dedicated staff. At Lugnutz Computer Parts, your satisfaction is our number one priority, and we encourage all of our customers (potential and returning) to reach out for honest support and recommendations, or to simply learn more about Larry's legacy!
+                    </p>
+                    <br>
+                    <h2>
+                        We appreciate your business and hope we can continue to fulfill Larry's vision for another 50 years!
+                    </h2>
+                </div>
             </div>
         </article>
     </section>
 </div>
-
 @endsection
