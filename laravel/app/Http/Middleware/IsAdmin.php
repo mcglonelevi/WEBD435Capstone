d7aa0165f35c8db1e171ff8aac89471a71dd8e9a
mcglonelevi <mcglonelevi@yahoo.com>
2017-11-02 20:09:25 -0400

    fix orders bug

--- laravel/app/Http/Middleware/IsAdmin.php
+++ laravel/app/Http/Middleware/IsAdmin.php
@@ -15,7 +15,7 @@ class IsAdmin
      */
     public function handle($request, Closure $next)
     {
-        if ($request->user()->is_admin) {
+        if ($request->user() && $request->user()->is_admin) {
             return $next($request);
         }
         abort(403, 'You must be an administrator to access this page.');
